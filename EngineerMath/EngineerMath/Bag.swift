2b5fb1d229fa2c959012ea2784f4a2073d02065c
Michael Griebling <michael.griebling@solinst.com>
2015-06-04 07:33:46 -0400

    Defined Bag data type.

--- EngineerMath/EngineerMath/Bag.swift
+++ EngineerMath/EngineerMath/Bag.swift
@@ -8,15 +8,59 @@
 
 import Foundation
 
-struct Bag<T:Hashable> {
+struct Bag<T:Hashable> : SequenceType {
 	
 	var items = [T: Int]()	// Bag definition as an object and a quantity
 	
 	var count : Int {
+		var itemCount = 0
+		for item in items { itemCount += item.1 }
+		return itemCount
+	}
+	
+	var uniqueCount : Int {
 		return items.count
 	}
 	
-	func add (item: T) {
-		
+	subscript (item: T) -> Int {
+		return items[item] ?? 0
 	}
+	
+	mutating func add (item: T) {
+		let value = items[item] ?? 0
+		items[item] = value+1
+	}
+	
+	mutating func removeOne (item: T) {
+		let value = items[item] ?? 0
+		if value == 0 { return }
+		if value == 1 {
+			items.removeValueForKey(item)
+		} else {
+			items[item] = value-1
+		}
+	}
+	
+	mutating func removeAll (item: T) {
+		let value = items[item] ?? 0
+		if value == 0 { return }
+		items.removeValueForKey(item)
+	}
+	
+	mutating func removeAllItems () {
+		items = [T: Int]()
+	}
+	
+	func isEmpty () -> Bool {
+		return count == 0
+	}
+	
+	// Sequence generator
+	func generate() -> DictionaryGenerator<T,Int> {
+		return items.generate()
+	}
+	
 }
+
+typealias UnitBag = Bag<String>
+
--- EngineerMath/EngineerMath/ViewController.swift
+++ EngineerMath/EngineerMath/ViewController.swift
@@ -20,28 +20,41 @@ class ViewController: NSViewController {
 //		println("1/a = \(b)")
 		
 		// BigCFloat
-//		var x : BigReal = 1
-//		var x = BigReal(1)
-		var x : BigComplex = 1 + 0.i
-		var y : BigComplex = 2.3456-2.5*i
-		var z = BigComplex("+5 - i23")
+////		var x : BigReal = 1
+////		var x = BigReal(1)
+//		var x : BigComplex = 1 + 0.i
+//		var y : BigComplex = 2.3456-2.5*i
+//		var z = BigComplex("+5 - i23")
+//		
+////		println("atan \(y)/\(x) = \(x.atan2(y))")
+//		println("x = \(x), y = \(y)")
+//		println("z = \(z); 1/z = \(1/z)")
+//		println("\(x) + \(y) = \(x+y)")
+//		println("\(x) - \(y) = \(x-y)")
+//		println("\(x) * \(y) = \(x*y)")
+//		println("\(x) / \(y) = \(x/y)")
+//		println("\(x) ** 2 = \(x ** 2)")
+//		println("\(x) ** 3 = \(x ** 3)")
+//		println("\(y) ** 2 = \(y ** 2)")
+//		println("\(y) ** 3 = \(y ** 3)")
+//		println("\(x) ** -1 = \(x ** -1)")
+//		println("2 ** 1000 = \(2 ** 1000)")
+//		if x > y { println("\(x) > \(y)") }
+//		else if x < y { println("\(x) < \(y)") }
+//		else { println("\(x) = \(y)") }
 		
-//		println("atan \(y)/\(x) = \(x.atan2(y))")
-		println("x = \(x), y = \(y)")
-		println("z = \(z); 1/z = \(1/z)")
-		println("\(x) + \(y) = \(x+y)")
-		println("\(x) - \(y) = \(x-y)")
-		println("\(x) * \(y) = \(x*y)")
-		println("\(x) / \(y) = \(x/y)")
-		println("\(x) ** 2 = \(x ** 2)")
-		println("\(x) ** 3 = \(x ** 3)")
-		println("\(y) ** 2 = \(y ** 2)")
-		println("\(y) ** 3 = \(y ** 3)")
-		println("\(x) ** -1 = \(x ** -1)")
-		println("2 ** 1000 = \(2 ** 1000)")
-		if x > y { println("\(x) > \(y)") }
-		else if x < y { println("\(x) < \(y)") }
-		else { println("\(x) = \(y)") }
+		var t = UnitBag()
+		
+		t.add("m"); t.add("m")
+		t.add("ft")
+//		t.removeAll("m")
+		
+		println("Bag contains \(t.count) items: ")
+		for (index, item) in enumerate(t) {
+			print("\(item)")
+			if index < t.uniqueCount-1 { print(", ") }
+		}
+		println()
 	}
 
 	override var representedObject: AnyObject? {
